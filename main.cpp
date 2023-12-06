6a5a0abfae165518e7199c93d6297f344dabe346
leoozzz <leonardo.heron@etu.univ-st-etienne.fr>
2023-12-06 18:01:21 +0100

    Implement some methods in LogManager & main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "LogManager.h"
 #include "Log.h"
 #include <iostream>
-#include <fstream>
 
 int main(int argc, char const *argv[])
 {
+    Stats stats;
+    LogManager logManager(argv[1]);
+    logManager.FillLog(stats);
     return 0;
 }
